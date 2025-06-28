3ada7f821a5315096f4a67b14056f1270b2db637
PauloLazaroAluno <paulo.lazaro@aluno.faculdadeimpacta.com.br>
2025-06-28 13:53:42 -0300

    Criação Inicial
    Criação da V1 Do Trabalho

---
+++ app.py
@@ -0,0 +1,23 @@
+# app.py
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template('index.html')
+
+@app.route("/sobre")
+def sobre():
+    return render_template('sobre.html')
+
+@app.route("/produtos")
+def produtos():
+    return render_template('produtos.html')
+
+@app.route("/contato")
+def contato():
+    return render_template('contato.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
